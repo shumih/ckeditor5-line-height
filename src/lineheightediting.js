8ee699521b72f4777a311816efe2d9a9e12812fe
Anton Shumikhin <shumih.anton@gmail.com>
2019-08-20 17:02:23 +0300

    support engine changes

--- src/lineheightediting.js
+++ src/lineheightediting.js
@@ -1,61 +1,55 @@
-import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
-import LineHeightCommand from './lineheightcommand';
-import { upcastElementToAttribute } from '@ckeditor/ckeditor5-engine/src/conversion/upcast-converters';
-import { normalizeOptions, buildDefinition } from './utils';
+import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
+import LineHeightCommand from "./lineheightcommand";
+import { UpcastHelpers } from "@ckeditor/ckeditor5-engine/src/conversion/upcasthelpers";
+import { normalizeOptions, buildDefinition } from "./utils";
 
-const LINE_HEIGHT = 'lineHeight';
+const LINE_HEIGHT = "lineHeight";
 
 export default class LineHeightEditing extends Plugin {
-	constructor( editor ) {
-		super( editor );
+  constructor(editor) {
+    super(editor);
 
-		editor.config.define( LINE_HEIGHT, {
-			options: [
-				1,
-				1.25,
-				1.5,
-				1.75,
-				2,
-				2.25,
-				2.5
-			]
-		} );
-	}
+    this.upcastHelpers = new UpcastHelpers();
+    editor.config.define(LINE_HEIGHT, {
+      options: [1, 1.25, 1.5, 1.75, 2, 2.25, 2.5]
+    });
+  }
 
-	/**
-	 * @inheritDoc
-	 */
-	init() {
-		const editor = this.editor;
+  /**
+   * @inheritDoc
+   */
+  init() {
+    const editor = this.editor;
 
-		// Allow LineHeight attribute on text nodes.
-		editor.model.schema.extend('$text', { allowAttributes: LINE_HEIGHT });
+    // Allow LineHeight attribute on text nodes.
+    editor.model.schema.extend("$text", { allowAttributes: LINE_HEIGHT });
 
-		// Define view to model conversion.
-		const options = normalizeOptions(this.editor.config.get('lineHeight.options')).filter(item => item.model);
-		const definition = buildDefinition(LINE_HEIGHT, options);
+    // Define view to model conversion.
+    const options = normalizeOptions(
+      this.editor.config.get("lineHeight.options")
+    ).filter(item => item.model);
+    const definition = buildDefinition(LINE_HEIGHT, options);
 
-		// Set-up the two-way conversion.
-		editor.conversion.attributeToElement(definition);
+    // Set-up the two-way conversion.
+    editor.conversion.attributeToElement(definition);
 
-		editor.conversion.for('upcast')
-			.add(upcastElementToAttribute({
-				view: {
-					name: 'span',
-					styles: {
-						'line-height': /^\d+(.\d+)?$/ // 非负浮点数
-					}
-				},
-				model: {
-					key: LINE_HEIGHT,
-					value: viewElement => {
-						const lineHeight = viewElement.getStyle('line-height');
-						return lineHeight;
-					}
-				}
-			}));
+    this.upcastHelpers.elementToAttribute({
+      view: {
+        name: "span",
+        styles: {
+          "line-height": /^\d+(.\d+)?$/ // 非负浮点数
+        }
+      },
+      model: {
+        key: LINE_HEIGHT,
+        value: viewElement => {
+          const lineHeight = viewElement.getStyle("line-height");
+          return lineHeight;
+        }
+      }
+    });
 
-		// Add LineHeight command.
-		editor.commands.add(LINE_HEIGHT, new LineHeightCommand(editor));
-	}
+    // Add LineHeight command.
+    editor.commands.add(LINE_HEIGHT, new LineHeightCommand(editor));
+  }
 }
